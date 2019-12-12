a605f4721b9b0801eedc7d4302693c96c93378b4
flohschaf <zjjr@arcor.de>
2019-12-12 14:42:05 +0100

    zrhr6uz

--- src/TestClass2.java
+++ src/TestClass2.java
@@ -3,7 +3,7 @@ public class TestClass2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hello World 21");
+		System.out.println("Hello World 2");
 	}
 
 }
